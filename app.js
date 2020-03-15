5a86d85564e50b7ea0d862dcf4eda31349494ab5
Simon <58277160+Ho-yeong@users.noreply.github.com>
2020-03-16 01:55:50 +0900

    route, controller setting
    C part of MVC

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ import helmet from "helmet"; //for security
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 //these two handle cookie and body
-import { userRouter } from "./router";
+// import { userRouter } from "./routers/userRouter";
 // it didn't be imported as default. that's why use {} bracket
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
 const PORT = 4000;
 
-const handleHome = (req, res) =>
-  //req = request object , res = response object
-  res.send("Hello from ass");
-
-const handleProfile = (req, res) => res.send("you are on my profile");
+//req = request object , res = response object
 
 app.use(cookieParser()); //this is how the server understands cookies coming from users
 app.use(bodyParser.json()); //this is how the server understands data coming from users
@@ -23,10 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
