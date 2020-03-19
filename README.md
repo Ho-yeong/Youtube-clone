f93d9382890b85b0dd7658ac5d241600758d2e80
Simon <58277160+Ho-yeong@users.noreply.github.com>
2020-03-19 21:52:24 +0900

    node JS
    made a fake db
    and show it on 'Home'

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Youtube-clone
 
 Cloning youtube with vanilaJS and nodeJS
+
+## page :
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 3422423,
+    title: "Video Awesome",
+    description: "This is what I love",
+    views: 24,
+    videoFile:
+      "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
+    creator: {
+      userid: "nothing",
+      name: "Simon",
+      email: "some@thing.com"
+    }
+  },
+  {
+    id: 34352,
+    title: "Video Perfect",
+    description: "This is what I love",
+    views: 24,
+    videoFile:
+      "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
+    creator: {
+      userid: "nothing",
+      name: "Simon",
+      email: "some@thing.com"
+    }
+  },
+  {
+    id: 678576,
+    title: "Video Good",
+    description: "This is what I love",
+    views: 24,
+    videoFile:
+      "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
+    creator: {
+      userid: "nothing",
+      name: "Simon",
+      email: "some@thing.com"
+    }
+  },
+  {
+    id: 12345,
+    title: "Video Nice",
+    description: "This is what I love",
+    views: 24,
+    videoFile:
+      "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
+    creator: {
+      userid: "nothing",
+      name: "Simon",
+      email: "some@thing.com"
+    }
+  }
+];
