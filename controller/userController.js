3a441319276d284be96a23d080a9490ce770e90f
Simon <58277160+Ho-yeong@users.noreply.github.com>
2020-03-23 18:20:46 +0900

    controller and routes

--- controller/userController.js
+++ controller/userController.js
@@ -21,9 +21,17 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Log out" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  //Process : to do Log out
+  res.redirect(routes.home);
+};
+
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
