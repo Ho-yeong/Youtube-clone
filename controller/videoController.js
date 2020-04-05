dbcaaee809df243bec52b606b376fe9048cb03a5
Simon <58277160+Ho-yeong@users.noreply.github.com>
2020-04-06 00:58:49 +0900

    installed Webpack

--- controller/videoController.js
+++ controller/videoController.js
@@ -10,15 +10,23 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
   //const searchingBy = req.query.term;
-
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", {
     pageTitle: "Search",
-    searchingBy: searchingBy
+    searchingBy: searchingBy,
+    videos
   });
 };
 
